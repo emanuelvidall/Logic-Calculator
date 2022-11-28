8b3d7453e3646dd1d811e907b3e6cf1e161cdeab
Emanuel Vidal <emanuelvidalrm@gmail.com>
2022-11-28 01:53:37 -0300

    changed problem approach
    Changed from react-vite to simple html and vanilla js for simplification of logic calculations.

--- src/components/Header/styles.ts
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const Title  = styled.header`
-        background: #7159c1;
-        height: 60px;
-        color: #fff;
-        display: flex;
-        align-items: center;
-    `;
